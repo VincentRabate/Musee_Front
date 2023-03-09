96d04897f199dd02dc531c80181d20aecc8de557
Vincent Rabate <118382789+VincentRabate@users.noreply.github.com>
2023-03-09 11:30:08 +0100

    aléatoire+reorganisation

--- src/App.vue
+++ src/App.vue
@@ -1,128 +1,6 @@
 <script setup>
 import { ref, computed } from "vue";
-
-const questions = ref([
-  {
-    answer: 0,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/CAR/lpdp_25745-1.jpg?itok=Qrqw80MA",
-    selected: null,
-  },
-  {
-    answer: 1,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/COG/25917-20.jpg?itok=YLWhV4Vr",
-    selected: null,
-  },
-  {
-    answer: 0,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_88075-6.jpg?itok=j0Htx44M",
-    selected: null,
-  },
-  {
-    answer: 2,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_72167-1.jpg?itok=V4AS1Q3M",
-    selected: null,
-  },
-  {
-    answer: 3,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_48739-2.jpg?itok=xqwsMCVY",
-    selected: null,
-  },
-  {
-    answer: 1,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/CAR/lpdp_33933-6.jpg?itok=PS62G2Pr",
-    selected: null,
-  },
-  {
-    answer: 3,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_26313-17.jpg?itok=elP5-sDx",
-    selected: null,
-  },
-  {
-    answer: 2,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_24769-9.jpg?itok=Xb3mTdd4",
-    selected: null,
-  },
-  {
-    answer: 0,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_82935-14.jpg?itok=Q4ciNVQv",
-    selected: null,
-  },
-  {
-    answer: 3,
-    options: [
-      "Gustave Courbet",
-      "Élisabeth Vigée Le Brun",
-      "Paul Cézanne",
-      "Mary Cassat",
-    ],
-    image:
-      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_33870-1.jpg?itok=qpJlcUbx",
-    selected: null,
-  },
-]);
+import questions from "./questions.js";
 
 const quizCompleted = ref(false);
 const currentQuestion = ref(0);
@@ -159,6 +37,12 @@ const NextQuestion = () => {
 
   quizCompleted.value = true;
 };
+
+const shuffleQuestions = () => {
+  questions.value.sort(() => Math.random() - 0.5);
+};
+
+shuffleQuestions();
 </script>
 
 <template>
@@ -217,8 +101,8 @@ const NextQuestion = () => {
       <p class="end-screen">
         Votre score est de {{ score }}/{{ questions.length }}
       </p>
-      <div v-if="score >= 6"><p>Pas mal bien joué</p></div>
-      <div v-else><p>essaie encore</p></div>
+      <div v-if="score >= 6"><p>Bien joué 😃</p></div>
+      <div v-else><p>Essaie encore 😉</p></div>
     </section>
   </main>
 </template>
---
+++ src/questions.js
@@ -0,0 +1,124 @@
+import { ref } from "vue";
+const questions = ref([
+  {
+    answer: 0,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/CAR/lpdp_25745-1.jpg?itok=Qrqw80MA",
+    selected: null,
+  },
+  {
+    answer: 1,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/COG/25917-20.jpg?itok=YLWhV4Vr",
+    selected: null,
+  },
+  {
+    answer: 0,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_88075-6.jpg?itok=j0Htx44M",
+    selected: null,
+  },
+  {
+    answer: 2,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_72167-1.jpg?itok=V4AS1Q3M",
+    selected: null,
+  },
+  {
+    answer: 3,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_48739-2.jpg?itok=xqwsMCVY",
+    selected: null,
+  },
+  {
+    answer: 1,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/CAR/lpdp_33933-6.jpg?itok=PS62G2Pr",
+    selected: null,
+  },
+  {
+    answer: 3,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_26313-17.jpg?itok=elP5-sDx",
+    selected: null,
+  },
+  {
+    answer: 2,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_24769-9.jpg?itok=Xb3mTdd4",
+    selected: null,
+  },
+  {
+    answer: 0,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_82935-14.jpg?itok=Q4ciNVQv",
+    selected: null,
+  },
+  {
+    answer: 3,
+    options: [
+      "Gustave Courbet",
+      "Élisabeth Vigée Le Brun",
+      "Paul Cézanne",
+      "Mary Cassat",
+    ],
+    image:
+      "https://apicollections.parismusees.paris.fr/sites/default/files/styles/thumbnail/collections/atoms/images/PPA/lpdp_33870-1.jpg?itok=qpJlcUbx",
+    selected: null,
+  },
+]);
+export default questions;
